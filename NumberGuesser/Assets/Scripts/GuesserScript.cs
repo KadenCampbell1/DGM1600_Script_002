39a308044eedc6ab98451a966d9a5be015d2930f
Unknown <10836246@my.uvu.edu>
2018-01-10 09:54:41 -0700

    Created NumberGuesser Game

---
+++ NumberGuesser/Assets/Scripts/GuesserScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuesserScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("our game has started");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

4d3b1819f91623f370fe357a806e58a2e5b26907
Unknown <10836246@my.uvu.edu>
2018-01-17 09:58:34 -0700

    more work done

--- NumberGuesser/Assets/Scripts/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts/GuesserScript.cs
@@ -2,26 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuesserScript : MonoBehaviour {
+public class GuesserScript : MonoBehaviour
+{
 
     public int max;
     public int min;
     public int guess;
 
 	// Use this for initialization
-	private void Start () {
+	private void Start ()
+    {
 
         print("Welcome to Number Guesser");
         print("Pick a number between " + min + " and " + max);
 
         // Is the value GUESS
-        print("Is the number 500?");
+        print("Is the number " + guess + "?");
         //Instructions - Push these buttons
         print("Up arrow for higher, Down arrow for lower, Enter for correct.");
 	}
 	
 	// Update is called once per frame
-	public void Update () {
-        
+	public void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (min + max) / 2;
+            print("Is the number " + guess + "?");
+        }
+
+        //Down arrow
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (min + max) / 2;
+            print("Is the number " + guess + "?");
+        }
+
+        //Enter button
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter)) 
+        {
+            print("I have won this time.");
+        }
+
 	}
 }

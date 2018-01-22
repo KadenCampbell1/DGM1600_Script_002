16db5c0f3938650c404f65647af889e20b647449
Unknown <10836246@my.uvu.edu>
2018-01-22 09:40:42 -0700

    created Text Adventure. Due Wed... make clue game in mansion

---
+++ TextAdventure/Assets/Scripts/adventure.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adventure : MonoBehaviour {
+
+
+    public enum Location { cell, window, gate};
+    public Location myLocation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

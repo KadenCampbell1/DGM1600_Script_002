1484f46f6e3356ea4c2745643c573151f4612a2d
Unknown <10836246@my.uvu.edu>
2018-02-02 09:55:34 -0700

    BrickBreaker added

---
+++ Brickbreaker/Assets/Script/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+    public Text title;
+    public Text startButton;
+    public Text quitButton;
+	// Use this for initialization
+	void Start () {
+        title.text = "Brick Breaker!";
+        startButton.text = "Play";
+        quitButton.text = "Quit";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

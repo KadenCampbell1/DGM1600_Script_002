88d466d7a252299f2f20f2c87da279678c8268d7
Unknown <10836246@my.uvu.edu>
2018-02-09 09:53:58 -0700

    brickBreaker Work

---
+++ Brickbreaker/Assets/Script/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    //Singleton
+
+    public static MusicManager instance = null;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+		
+        if (instance == null)                               //if instance is not assigned
+        {                                                   //then assign instance to this object
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);                       //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+	}
+	
+
+}

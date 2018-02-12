bc0bc0cb662ba28cd6e27cec3d0c8135aa91a4e5
Unknown <10836246@my.uvu.edu>
2018-02-12 09:49:08 -0700

    brickBreaker work 2/12

---
+++ Brickbreaker/Assets/Script/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+    
+	void Update ()
+    {
+        //get current paddle position
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+
+        //get mouse horizontal position
+        float mouseX = Input.mousePosition.x / Screen.width * 16 - 8;
+     
+        paddlePosition.x = mouseX;
+
+        //set this paddle to saved position
+        //can have this. in front
+        gameObject.transform.position = paddlePosition;
+	}
+}

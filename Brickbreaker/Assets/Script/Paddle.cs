dbcb4ad31a5e1b891030e00aa254d51257d920e9
Unknown <10836246@my.uvu.edu>
2018-02-14 09:54:24 -0700

    brickBreaker work 2/14 need to add hitSound

--- Brickbreaker/Assets/Script/Paddle.cs
+++ Brickbreaker/Assets/Script/Paddle.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    public bool autoPlay;
+    private GameObject ball;
 
-    
-	void Update ()
+
+    private void Start()
+    {
+        ball = FindObjectOfType<Ball>().gameObject;
+
+    }
+
+    void Update ()
     {
         //get current paddle position
         Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
 
+        if (autoPlay)
+        {
+            paddlePosition.x = ball.transform.position.x;
+        }
+        else
+        {
+            float mouseX = Input.mousePosition.x / Screen.width * 16 - 8;
+
+            paddlePosition.x = mouseX;
+        }
         //get mouse horizontal position
-        float mouseX = Input.mousePosition.x / Screen.width * 16 - 8;
-     
-        paddlePosition.x = mouseX;
+        
 
         //set this paddle to saved position
         //can have this. in front

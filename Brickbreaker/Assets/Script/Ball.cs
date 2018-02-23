0e8489ba0676093eb8e1c0d115e10d12ed13dd79
Unknown <10836246@my.uvu.edu>
2018-02-23 10:01:08 -0700

    Brick Breaker work 2/23 do variables homework

--- Brickbreaker/Assets/Script/Ball.cs
+++ Brickbreaker/Assets/Script/Ball.cs
@@ -6,6 +6,12 @@ public class Ball : MonoBehaviour {
 
 
     public AudioClip hitSound;
+    public Vector2 startVector;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(startVector, ForceMode2D.Force);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- Brickbreaker/Assets/Script/Brick.cs
+++ Brickbreaker/Assets/Script/Brick.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+public class Brick : MonoBehaviour
+{
     public int health;
+    public Sprite[] sprites;
 
     private void Awake()
     {
         GameManager.brickCount++;
         print(GameManager.brickCount);
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         health--;
-        
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+
         //if our health gets to zero then destroy
         if (health <= 0)
         {

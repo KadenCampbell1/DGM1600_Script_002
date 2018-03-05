4d81e7331dabbf3e99b41579a4bad45323b97500
Unknown <10836246@my.uvu.edu>
2018-03-05 10:01:03 -0700

    inclass final work 3/5

--- FinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovement : MonoBehaviour {
 
 
     private Rigidbody2D rigid;
     public float speed;
+    private Animator anim;
+    private SpriteRenderer rend;
 
 	// Use this for initialization
 	void Start ()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +28,19 @@ public class PlayerMovement : MonoBehaviour {
         //check for button pushes
 
         rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") *speed), ForceMode2D.Force);
+        //to cast as an integer put (int) in front of Input
+        anim.SetFloat("HorizontalGo", Input.GetAxisRaw("Horizontal"));
 
+        if(Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            //flip sprite renderer
+            rend.flipX = true;
+        }
+        else
+        {
+            //unflip
+            rend.flipX = false;
+        }
 
 	}
 }

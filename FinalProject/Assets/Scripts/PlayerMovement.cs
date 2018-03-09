16c18eb18279668b6ffdd39e007c4579c93f1043
Unknown <10836246@my.uvu.edu>
2018-03-09 09:55:32 -0700

    inclass Final work 3/9

--- FinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;
     private Animator anim;
     private SpriteRenderer rend;
+    public float jump;
+    private bool isGrounded;
 
 	// Use this for initialization
 	void Start ()
@@ -47,5 +49,26 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetTrigger("ShootGo");
         }
 
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(new Vector2(0, jump), ForceMode2D.Force);
+        }
+
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }

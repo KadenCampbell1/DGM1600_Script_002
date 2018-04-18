45875b0231f917d9f5654c2912379cd1d5bda2ff
Unknown <10836246@my.uvu.edu>
2018-04-18 05:23:34 -0600

    final work

--- Hotel Mystery/Assets/Scripts/ChangeComputerBubble.cs
+++ Hotel Mystery/Assets/Scripts/ChangeComputerBubble.cs
@@ -34,17 +34,17 @@ public class ChangeComputerBubble : MonoBehaviour {
 
         if (textNumber == 4)
         {
-            textBubble.text = "Excuse me, Lobby boy.";
+            textBubble.text = "Yo, Lobby boy.";
         }
 
         if (textNumber == 5)
         {
-            textBubble.text = "Excuse me, Lobby boy.";
+            textBubble.text = "Excuse me, could you help me.";
         }
 
         if (textNumber == 6)
         {
-            textBubble.text = "Excuse me, Lobby boy.";
+            textBubble.text = "Hey, yo bud.";
         }
     }
 
@@ -56,10 +56,12 @@ public class ChangeComputerBubble : MonoBehaviour {
                 "It is disturbing me... Could you take my stuff up to my room and report back?\n" +
                 "There will be a reward in it for you.\n" +
                 "It is room 110.\n" +
-                "I want to get to there as soon as possible.";
+                "I want to get out of here as soon as possible.";
 
             //drop item that corrisponds to first computer and check for variable deservesReward to drop money
             // purse
+            // for good bye text make it say "thanks I'll be heading out then"
+            // use enable and disable to control where things spawn in
         }
 
         if (textNumber == 1)
@@ -75,7 +77,7 @@ public class ChangeComputerBubble : MonoBehaviour {
         if (textNumber == 2)
         {
             textBubble.text = "If you would go and take my BACKPACK to my room.\n" +
-                "It is room 207.";
+                "It is room 207. Come back after and I'll reward you.";
 
             //drop item that corrisponds to first computer and check for variable deservesReward to drop money
             //Backpack
@@ -94,27 +96,30 @@ public class ChangeComputerBubble : MonoBehaviour {
 
         if (textNumber == 4)
         {
-            textBubble.text = "There was a murder in the center of your lobby.\n" +
-                "It is disturbing me... Could you take my stuff up to my room?\n" +
-                "I want to get to my room as soon as possible.";
+            textBubble.text = "Can you take my SUIT to room 101?\n" +
+                "I don't want it to wrinkle. Thanks my boy.\n" +
+                "Let me know when you're done and I'll give you something.";
 
             //drop item that corrisponds to first computer and check for variable deservesReward to drop money
+            //suit
         }
 
         if (textNumber == 5)
         {
-            textBubble.text = "There was a murder in the center of your lobby.\n" +
-                "It is disturbing me... Could you take my stuff up to my room?\n" +
-                "I want to get to my room as soon as possible.";
+            textBubble.text = "Oh good, Lobby boy, my room is 305.\n" +
+                "Would you mind taking my BOOK to my room\n" +
+                "and letting me know when you are done?\n" +
+                "I would like to leave until this is cleaned up.";
 
             //drop item that corrisponds to first computer and check for variable deservesReward to drop money
+            //Book
         }
 
         if (textNumber == 6)
         {
-            textBubble.text = "There was a murder in the center of your lobby.\n" +
-                "It is disturbing me... Could you take my stuff up to my room?\n" +
-                "I want to get to my room as soon as possible.";
+            textBubble.text = "Yo, Yo, Yo. Lobby boy I'm glad you shuffled over.\n" +
+                "I need you to run my BOARD up to my room. \n" +
+                "If you hurry and then come back I'll give you a reward.";
 
             //drop item that corrisponds to first computer and check for variable deservesReward to drop money
         }

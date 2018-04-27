88abc2e3959ccb8f61da8755ba7e31cd185345d9
Unknown <10836246@my.uvu.edu>
2018-04-27 10:16:53 -0600

    Fixed UI and item problem

--- Hotel Chaser/Assets/Scripts/ItemEffect.cs
+++ Hotel Chaser/Assets/Scripts/ItemEffect.cs
@@ -11,42 +11,49 @@ public class ItemEffect : MonoBehaviour {
 	
 	void Start () {
         npcScript = GameObject.Find("NPCComputer").GetComponent<NPCSpeechBubbles>();
-        playerScript = GetComponent<PlayerMovement>();
+        playerScript = GameObject.Find("Player 1").GetComponent<PlayerMovement>();
         musicManager = GameObject.Find("MusicManager").GetComponent<MusicManager>();
 	}
 
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.gameObject.CompareTag("Item"))
+        if(collider.gameObject.CompareTag("Player"))
         switch (npcScript.myNPC)
         {
             case NPCSpeechBubbles.npcLevel.Level01:
                 playerScript.speed += 10;
+                Destroy(this.gameObject);
                 break;
             case NPCSpeechBubbles.npcLevel.Level02:
                 playerScript.speed -= 3;
+                Destroy(this.gameObject);
                 break;
             case NPCSpeechBubbles.npcLevel.Level03:
                 playerScript.healthScript.health += 1;
+                Destroy(this.gameObject);
                 break;
             case NPCSpeechBubbles.npcLevel.Level04:
-                    playerScript.jump += 200;
+                playerScript.jump += 200;
+                Destroy(this.gameObject);
                 break;
             case NPCSpeechBubbles.npcLevel.Level05:
-                    playerScript.speed += 20;
-                    musicManager.Metal();
-                break;
+                playerScript.speed += 20;
+                musicManager.Metal();
+                Destroy(this.gameObject);
+                    break;
             case NPCSpeechBubbles.npcLevel.Level06:
-                    playerScript.speed += 5;
-                    playerScript.jump += 100;
-                    musicManager.Jazz();
+                playerScript.speed += 5;
+                playerScript.jump += 100;
+                musicManager.Jazz();
+                Destroy(this.gameObject);
                 break;
             case NPCSpeechBubbles.npcLevel.Level07:
-                    playerScript.speed -= 5;
-                    playerScript.jump -= 70;
-                    playerScript.healthScript.health += 7;
-                    break;
+                playerScript.speed -= 5;
+                playerScript.jump -= 70;
+                playerScript.healthScript.health += 7;
+                Destroy(this.gameObject);
+                break;
         }
     }
     
